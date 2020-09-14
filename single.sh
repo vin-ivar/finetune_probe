248a0e5d5ccfe27afe6f7e15ecfdc0904d2ea532
vin <vinit.ravishankar@gmail.com>
2020-09-14 15:00:07 +0200

    akh fuck

--- single.sh
+++ single.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-sbatch -J u.$2.$3 -e experiments/logs/underparam/$2.$3.log -o experiments/logs/underparam/$2.$3.log ft.slurm $1 $2 $3 $4
+sbatch -J u.$2.$3 -e experiments/logs/underparam/$2.$3.log -o experiments/logs/underparam/$2.$3.log ft.slurm $1 $2 $3
