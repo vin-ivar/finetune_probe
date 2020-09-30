84e3ab6eef46aedbbc8c39e90411abf470a68225
vin <vinit.ravishankar@gmail.com>
2020-09-30 12:29:48 +0200

    only_net

--- model.py
+++ model.py
@@ -194,6 +194,9 @@ class BiaffineDependencyParser(Model):
             params_to_freeze = [(k, v) for (k, v) in self.named_parameters()
                                 if 'value' not in k and '_head_sentinel' not in k]
 
+        if freeze == 'only_net':
+            params_to_freeze = [(k, v) for (k, v) in self.named_parameters() if k.startswith('text_field_embedder')]
+
         if freeze == 'kq_net':
             params_to_freeze = [(k, v) for (k, v) in self.text_field_embedder.named_parameters()
                                 if 'key' not in k and 'query' not in k]
