27bd790bb273e85ac2afa65c74a2832f983fe8d7
vin <vinit.ravishankar@gmail.com>
2020-10-22 19:53:27 +0200

    re-add prefix

--- single.sh
+++ single.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-sbatch -J $2.$3.$4 -e experiments/logs/naacl/$2.$3.$4.log -o experiments/logs/naacl/$2.$3.$4.log ft.slurm $1 $2 $3 $4
+sbatch -J $5.$2.$3.$4 -e experiments/logs/naacl/$5.$2.$3.$4.log -o experiments/logs/naacl/$5.$2.$3.$4.log ft.slurm $1 $2 $3 $4 $5
