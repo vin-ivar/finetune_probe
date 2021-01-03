a1a33a33f0b9bf36d55cf4ecc182be641dbba952
vin <vinit.ravishankar@gmail.com>
2021-01-03 16:05:22 +0100

    oops

--- single.sh
+++ single.sh
@@ -3,4 +3,4 @@ mkdir -p experiments/logs
 mkdir -p experiments/stats
 mkdir -p $SCRATCH/naacl
 
-sbatch -J $1.$2.$3 -e experiments/logs/$1.$2.$3.log -o experiments/logs/naacl/$1.$2.$3.log ft.slurm $1 $2 $3
+sbatch -J $1.$2.$3 -e experiments/logs/$1.$2.$3.log -o experiments/logs/$1.$2.$3.log ft.slurm $1 $2 $3
