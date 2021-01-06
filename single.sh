6b77f07e36e1b82b354f619b0cba8bb595995b51
Vinit Ravishankar <ravishan@puhti-login2.bullx>
2021-01-06 18:18:56 +0200

    gitignore logs

--- single.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-mkdir -p experiments/logs/$4
-mkdir -p experiments/stats
-mkdir -p $SCRATCH/naacl
-
-sbatch -J $1.$2.$3 -e experiments/logs/$4/$1.$2.$3.log -o experiments/logs/$4/$1.$2.$3.log ft.slurm $1 $2 $3
