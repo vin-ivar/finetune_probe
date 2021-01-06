17b650e730d37e0f0e40a5878177bbb0cba45007
Vinit Ravishankar <ravishan@puhti-login2.bullx>
2021-01-06 17:32:20 +0200

    updates

---
+++ experiments/scripts/component.sh
@@ -0,0 +1,8 @@
+cd ../..
+for lang in en_ewt it_isdt sv_talbanken; do 
+	for component in dense; do 
+		for layer in 0 0-1 0-2 0-6 0-7 0-10 0-11; do 
+			./single.sh $lang kill.$component.$layer none component
+		done
+	done
+done
