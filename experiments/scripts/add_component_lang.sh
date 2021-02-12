8e0f5f5316201757203eeb54a78f3f8901b96e88
Vinit Ravishankar <ravishan@puhti-login2.bullx>
2021-02-12 21:47:31 +0200

    initial

--- experiments/scripts/add_component_lang.sh
+++ experiments/scripts/add_component_lang.sh
@@ -1,6 +1,6 @@
 cd ../..
 for lang in zh_gsd ja_gsd ar_padt hi_hdtb; do 
-	for component in keys queries; do 
+	for component in values attn_dense; do 
 		for layer in 0 0-1 0-2 0-6 0-7 0-10 0-11; do 
 			./submit.sh $lang kill.$component.$layer none component
 		done
